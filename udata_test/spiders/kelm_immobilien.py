191b5c9741142f333da82d17ff5dca695cc6650f
Slava Loievskyi <loievskyi.slava@gmail.com>
2024-07-13 22:22:08 +0300

    feat: Update KelmImmobilienSpider to parse property details
    The spider KelmImmobilienSpider has been extended to include the parsing functionality of property details. New import statements and methods have been added for this purpose. Now, it extracts item details such as title, status, pictures, rent price, phone number, and email, among others.

--- udata_test/spiders/kelm_immobilien.py
+++ udata_test/spiders/kelm_immobilien.py
@@ -1,7 +1,11 @@
-from typing import Any
+import pprint
+import re
+from typing import Any, List
 
 import scrapy
 from scrapy.http import Response
+
+from udata_test.items import RentalItem
 
 
 class KelmImmobilienSpider(scrapy.Spider):
@@ -23,4 +27,80 @@ class KelmImmobilienSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
     def parse_details(self, response: Response, **kwargs: Any) -> Any:
-        pass
+        item = RentalItem(
+            url=response.url,
+            title=self._get_title(response),
+            status=self._get_status(response),
+            pictures=self._get_pictures(response),
+            rent_price=self._get_rent_price(response),
+            description=self._get_description(response),
+            phone_number=self._get_phone_number(response),
+            email=self._get_email(response),
+        )
+        pprint.pprint(item)
+        return item
+
+    def _get_title(self, response: Response) -> str | None:
+        return response.css("h1.property-title::text").get()
+
+    def _get_status(self, details_response: Response) -> str | None:
+        return (details_response
+                .css("li.list-group-item.data-vermietet .dd::text")
+                .get())
+
+    def _get_pictures(self, details_response: Response) -> List[str]:
+        return (details_response
+                .css("div#immomakler-galleria a img::attr(data-big)")
+                .getall())
+
+    def _convert_price(self, price: str) -> float | None:
+        try:
+            price = price.replace(".", "")
+            price = re.sub(r"[^\d.,]", "", price)
+            return float(price)
+        except Exception:
+            return None
+
+    def _get_purchase_price(self, details_response: Response) -> str | None:
+        price = (details_response
+                 .css("li.list-group-item.data-kaufpreis .dd::text")
+                 .get())
+        if price is not None:
+            return self._convert_price(price)
+        return None
+
+    def _get_rent_price(self, details_response: Response) -> str | None:
+        price = (details_response
+                 .css("li.list-group-item.data-kaltmiete .dd::text")
+                 .get())
+        if price is not None:
+            return self._convert_price(price)
+        return None
+
+    def _get_description(self, details_response: Response) -> str | None:
+        description = details_response.css(
+            "div.property-description .panel-body h3, "
+            "div.property-description .panel-body p")
+        description_text = ""
+        tag_header = ""
+        tag_text = ""
+        for description_part in description:
+            if description_part.root.tag == "h3":
+                if tag_text:
+                    description_text += f"{tag_header}\n{tag_text.strip()}\n"
+                tag_header = description_part.css("::text").get()
+                tag_text = ""
+            elif description_part.root.tag == "p":
+                tag_text += f"{description_part.css('::text').get().strip()} "
+        description_text += f"{tag_header}\n{tag_text.strip()}"
+        return description_text
+
+    def _get_phone_number(self, details_response: Response) -> str | None:
+        return (details_response
+                .css(".property-contact .dd.col-sm-7.p-tel.value a::text")
+                .get())
+
+    def _get_email(self, details_response: Response) -> str | None:
+        return (details_response
+                .css(".property-contact .dd.col-sm-7.u-email.value a::text")
+                .get())
